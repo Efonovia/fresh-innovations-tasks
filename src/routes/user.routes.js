0eeaa312dbced169105116601b53e30e705c2373
Efonovia <lordnovia@gmail.com>
2024-08-30 16:02:39 +0100

    edited route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,7 +17,7 @@ UsersRouter.post("/signup", createNewUser)
 UsersRouter.post("/login", loginUser)
 UsersRouter.patch("/edit/main-details", checkUserExistence, editUserMainDetails)
 UsersRouter.patch("/edit/password", checkUserExistence, changeUserPassword)
-UsersRouter.get("/user/id/:userId", checkUserExistence, getUser)
+UsersRouter.get("/id/:userId", checkUserExistence, getUser)
 
 
 export default UsersRouter
