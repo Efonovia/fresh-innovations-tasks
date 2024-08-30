e2e239e7e5ccb0f3d3c706c9744fe95d57416a36
Efonovia <lordnovia@gmail.com>
2024-08-30 16:44:51 +0100

    added middlewares

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -13,6 +13,6 @@ const TransactionsRouter = express.Router()
 TransactionsRouter.post("/create", checkUserExistence, createNewTransaction)
 TransactionsRouter.get("/get/:transactionNo", getUserTransactionByNo)
 TransactionsRouter.get("/all", getAllTransactions)
-TransactionsRouter.get("/all/user/:userId", getAllUserTransactions)
+TransactionsRouter.get("/all/user/:userId", checkUserExistence, getAllUserTransactions)
 
 export default TransactionsRouter
